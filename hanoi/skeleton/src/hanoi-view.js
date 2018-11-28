581cc7b83ca35cacd04dbaa7208dd1a9ea0bd31f
Emmad5 <emmadeas9@gmail.com>
2018-11-27 17:28:39 -0800

    Create styling for leaning towers of hanoi

--- hanoi/skeleton/src/hanoi-view.js
+++ hanoi/skeleton/src/hanoi-view.js
@@ -6,7 +6,10 @@ class HanoiView {
   }
   setupTowers() {
     const $board = $("<div>");
+    $board.addClass('board');
+    console.log('making a board');
     for (var i = 0; i < 3; i++) {
+      console.log('making a tower');
       const $tower = $("<ul>");
       $tower.addClass('tower');
       for (var j = 0; j < 3; j++) {
@@ -18,7 +21,8 @@ class HanoiView {
       }
       $board.append($tower);
     }
+    this.$el.append($board);
   }
 }
 
-module.exports = HanoiView; 
+module.exports = HanoiView;
